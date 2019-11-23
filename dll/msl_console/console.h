6598c3b46464b54014c34bb2fca470979455766d
#Momo <40578274+MomoDeve@users.noreply.github.com>
2019-11-23 19:34:03 +0300

    Create console.h

---
+++ dll/msl_console/console.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../LibMSL/msl_types.h"
+#include "../LibMSL/msl_types.cpp"
+#include "../LibMSL/bigInteger.cpp"
+
+#define DLLEXPORT extern "C" __declspec(dllexport) void _cdecl
+#define VM_ATTRIBUTES Stack* stack, AssemblyType* assembly, uint32_t* errors, Configuration* config, GarbageCollector* gc
+
+using namespace MSL::VM;
+using Stack = std::vector<MSL::VM::BaseObject*>;
+
+DLLEXPORT Print    (VM_ATTRIBUTES);
+DLLEXPORT PrintLine(VM_ATTRIBUTES);
+DLLEXPORT Read     (VM_ATTRIBUTES);
+DLLEXPORT ReadInt  (VM_ATTRIBUTES);
+DLLEXPORT ReadFloat(VM_ATTRIBUTES);
+DLLEXPORT ReadLine (VM_ATTRIBUTES);
+DLLEXPORT ReadBool (VM_ATTRIBUTES);
